03f429b0727f92397083d7e167dd2c3cc3b16f13
Keernom <keernom.gdev@gmail.com>
2021-12-26 00:50:11 +0300

    Coroutines added in script

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> _path = new List<Waypoint>();
+    float _waitTime = 1f;
+    private void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in _path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+}
