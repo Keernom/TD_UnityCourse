636db1c92e877c7ff09d93d93c8f9fb0b02c7746
Keernom <keernom.gdev@gmail.com>
2021-12-27 01:52:12 +0300

    Mouse detecting added

--- Assets/Tile/Waypoint.cs
+++ Assets/Tile/Waypoint.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool _isPlaceble;
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (_isPlaceble)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
