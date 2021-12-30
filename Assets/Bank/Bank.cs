14ae998c100e4fc3b39d5afd6c032b7d8a04d09f
Keernom <keernom.gdev@gmail.com>
2021-12-30 15:11:53 +0300

    UI gold display added

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
@@ -9,25 +10,35 @@ public class Bank : MonoBehaviour
     [SerializeField] int _currentBalance;
     public int CurrentBalace { get {return _currentBalance;} }
 
+    [SerializeField] TextMeshProUGUI _displayBalance;
 
     private void Awake()
     {
         _currentBalance = _startBalance;
+        DisplayUpdate();
     }
 
     public void Deposit(int amount)
     {
         _currentBalance += Mathf.Abs(amount);
+        DisplayUpdate();
     }
 
     public void WithDraw(int amount)
     {
         _currentBalance -= Mathf.Abs(amount);
+        DisplayUpdate();
 
         if (_currentBalance < 0)
         {
             ReloadScene();
         }
+    }
+
+
+    void DisplayUpdate()
+    {
+        _displayBalance.text = "Gold: " + _currentBalance;
     }
 
     private static void ReloadScene()
