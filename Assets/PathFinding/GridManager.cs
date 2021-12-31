dc212024a39a8be02b5e077e930612e7e94bceaf
Keernom <keernom.gdev@gmail.com>
2021-12-31 16:06:34 +0300

    dictionary added

--- Assets/PathFinding/GridManager.cs
+++ Assets/PathFinding/GridManager.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Node _node;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(_node._coordiantes);
-        Debug.Log(_node._isWalkable);
-    }
+    [SerializeField] Vector2Int _gridSize;
+    Dictionary<Vector2Int, Node> _grid = new Dictionary<Vector2Int, Node>();
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        for(int x = 0; x < _gridSize.x; x++)
+        {
+            for(int y = 0; y < _gridSize.y; y++)
+            {
+                Vector2Int _coordinates = new Vector2Int(x, y);
+                _grid.Add(_coordinates, new Node(_coordinates, true));
+                Debug.Log(_grid[_coordinates]._coordiantes + " = " + _grid[_coordinates]._isWalkable);
+            }
+        }
     }
 }
