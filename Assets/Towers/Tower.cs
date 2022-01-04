b982effb49f09fad2c7ba111ef8cab342bf6c98d
Keernom <keernom.gdev@gmail.com>
2022-01-04 16:47:24 +0300

    buildTimer added

--- Assets/Towers/Tower.cs
+++ Assets/Towers/Tower.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int _cost = 75;
+    [SerializeField] int _buildDelay = 1;
+    bool _isTowerActive = true;
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -23,5 +29,28 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in transform)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(_buildDelay);
+            foreach (Transform grandchild in transform)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
+        Debug.Log("HELLO - CORURINE");
     }
 }
